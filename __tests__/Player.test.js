7cd1d02ab73ad1077fa6408c1ea3a5892fcf7b66
Dave Sanders <davepaulsanders@gmail.com>
2022-04-20 08:17:10 -0400

    Player object prototype methods added

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -4,7 +4,7 @@ jest.mock("../lib/Potion");
 
 test("creates a player object", () => {
   const player = new Player("Dave");
-    
+
   expect(player.name).toBe("Dave");
   expect(player.health).toEqual(expect.any(Number));
   expect(player.strength).toEqual(expect.any(Number));
@@ -13,3 +13,22 @@ test("creates a player object", () => {
     expect.arrayContaining([expect.any(Object)])
   );
 });
+
+test("gets player's stats as an object", () => {
+    const player = new Player('Dave');
+  
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+  });
+
+  test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+  
+    expect(player.getInventory()).toEqual(expect.any(Array));
+  
+    player.inventory = [];
+  
+    expect(player.getInventory()).toEqual(false);
+  });
